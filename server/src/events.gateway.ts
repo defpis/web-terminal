7f525d3ad8ef7a3fdb1cf230a6dd4a7e9a0a273a
wenjun.super <wenjun.super@bytedance.com>
2025-04-10 17:39:07 +0800

    add xterm

--- server/src/events.gateway.ts
+++ server/src/events.gateway.ts
@@ -2,8 +2,40 @@ import {
   WebSocketGateway,
   WebSocketServer,
   SubscribeMessage,
+  ConnectedSocket,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import * as pty from 'node-pty';
+
+function spawn(socket: Socket) {
+  const term = pty.spawn('zsh', [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    env: process.env,
+    cwd: process.env.HOME,
+  });
+
+  term.onExit(() => {
+    socket.removeAllListeners();
+  });
+  term.onData((data: string) => {
+    socket.emit('output', data);
+  });
+
+  socket.on('resize', (size: { cols: number; rows: number }) => {
+    term.resize(size.cols, size.rows);
+  });
+  socket.on('input', (data: string) => {
+    term.write(data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`客户端 ${socket.id} 断开连接`);
+    term.kill();
+  });
+}
 
 @WebSocketGateway({
   cors: { origin: '*' },
@@ -13,13 +45,17 @@ export class EventsGateway {
   server: Server;
 
   @SubscribeMessage('message')
-  handleMessage(client: Socket, message: string) {
-    console.log(`客户端 ${client.id} 发送: ${message}`);
-    client.emit('message', message);
+  handleMessage(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() message: string,
+  ) {
+    console.log(`客户端 ${socket.id} 发送: ${message}`);
+    socket.emit('message', message);
     return { status: 'success' };
   }
 
-  handleConnection(client: Socket) {
-    console.log(`客户端 ${client.id} 连接`);
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    console.log(`客户端 ${socket.id} 连接`);
+    spawn(socket);
   }
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,64 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { io, Socket } from "socket.io-client";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
 
 function App() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
 
   const socketRef = useRef<Socket>(null);
+  const xtermRef = useRef<Terminal>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const socket = (socketRef.current = io("http://localhost:3000"));
 
-    socket.on("message", (data) => {
-      setMessages((prev) => [...prev, data]);
+    socket.on("message", (message) => {
+      setMessages((prev) => [...prev, message]);
+    });
+    socket.on("output", (data) => {
+      xtermRef.current?.write(data);
+    });
+
+    const xterm = (xtermRef.current = new Terminal({
+      cols: 80,
+      rows: 30,
+      theme: {
+        background: "#222",
+        foreground: "#fff",
+      },
+    }));
+
+    xterm.onResize(() => {
+      socket.emit("resize", { cols: xterm.cols, rows: xterm.rows });
+    });
+    xterm.onData((data) => {
+      socket.emit("input", data);
+    });
+
+    const fitAddon = new FitAddon();
+    xterm.loadAddon(fitAddon);
+
+    const observer = new ResizeObserver(() => {
+      fitAddon.fit();
+    });
+
+    // 解决 Uncaught TypeError: Cannot read properties of undefined (reading 'dimensions')
+    const rafId = requestAnimationFrame(() => {
+      if (!containerRef.current) return;
+      xterm.open(containerRef.current);
+      observer.observe(containerRef.current); // 触发一次 fit
     });
 
     return () => {
       socket.disconnect();
       socketRef.current = null;
+      xterm.dispose();
+      xtermRef.current = null;
+      observer.disconnect();
+      cancelAnimationFrame(rafId);
     };
   }, []);
 
@@ -32,11 +73,11 @@ function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className="terminal-container" ref={containerRef}></div>
 
       <div>
         <input value={message} onChange={(e) => setMessage(e.target.value)} />
-        <button onClick={send}>发送</button>
+        <button onClick={send}>Send</button>
       </div>
 
       <div>
